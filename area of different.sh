d6ee47c7cb23d27f760e35fc59ee36566b5965dd
SwarnalakshmiTS <112744358+SwarnalakshmiTS@users.noreply.github.com>
2023-02-13 14:01:22 +0530

    Create area of different.sh

---
+++ area of different.sh
@@ -0,0 +1,14 @@
+echo "enter the side of the square"
+read s
+echo "enter the length and breadth of the rectangle"
+read leng
+read brea
+echo "enter the radius of the circle"
+read radius
+echo "area of square is:`expr $s \* $s`"
+echo "area of rectangle is:`expr $leng \* $brea`"
+echo "area of the circle is: `expr 3.14 \* radius \* radius`"
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
